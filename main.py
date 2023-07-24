e261ec6f681836a087095007ccbfbff7d293b919
devshah21 <mail2devshah@gmail.com>
2023-07-24 12:01:55 -0400

    defined an LSTM model and remade the model

--- main.py
+++ main.py
@@ -1,34 +1,18 @@
 import streamlit as st
-from datetime import date
 import yfinance as yf
-from prophet import Prophet
+import pandas as pd
+import numpy as np
 import plotly.graph_objs as go
+from sklearn.preprocessing import MinMaxScaler
+import tensorflow
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense
+from tensorflow.keras.optimizers import Adam
+from datetime import date
 
 START = "2019-01-01"
 TODAY = date.today().strftime("%Y-%m-%d")
 
-# Custom CSS style for the slider
-CUSTOM_CSS = """
-<style>
-/* Custom Streamlit slider style */
-.st-eb {
-    color: #555;
-    font-size: 16px;
-}
-.st-f6 {
-    color: #555;
-    font-size: 14px;
-}
-/* Custom Streamlit slider track color */
-.st-c0 {
-    background-color: #ddd;
-}
-/* Custom Streamlit slider thumb color */
-.st-c4 {
-    background-color: #007BFF;
-}
-</style>
-"""
 
 def load_data(ticker):
     data = yf.download(ticker, START, TODAY)
@@ -55,32 +39,45 @@ def plot_raw_data(data):
     st.plotly_chart(fig)
 
 
-def plot_forecast_data(forecast):
-    # Plotting forecast data
-    fig = go.Figure()
+def prepare_data(df_train):
+    # Find the column with numeric values (assumed to be the 'Close' column)
+    close_col_name = None
+    for col in df_train.columns:
+        if np.issubdtype(df_train[col].dtype, np.number):
+            close_col_name = col
+            break
 
-    fig.add_trace(go.Scatter(x=forecast['ds'], y=forecast['yhat'], mode='lines', name='Forecast', line=dict(color='royalblue')))
-    fig.add_trace(go.Scatter(x=forecast['ds'], y=forecast['yhat_lower'], fill='tonexty', mode='none', name='Lower Bound', fillcolor='rgba(135, 206, 250, 0.2)'))
-    fig.add_trace(go.Scatter(x=forecast['ds'], y=forecast['yhat_upper'], fill='tonexty', mode='none', name='Upper Bound', fillcolor='rgba(135, 206, 250, 0.2)'))
+    if close_col_name is None:
+        raise ValueError("No numeric column found in the data for 'Close' price.")
 
-    fig.update_layout(
-        title_text='Stock Price Forecast',
-        xaxis_title='Date',
-        yaxis_title='Price',
-        showlegend=True,
-        legend=dict(x=0, y=1),
-        xaxis_rangeslider_visible=False,
-        xaxis=dict(showgrid=True),
-        yaxis=dict(showgrid=True),
-    )
+    # Normalize the data
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    scaled_data = scaler.fit_transform(df_train[close_col_name].values.reshape(-1, 1))
 
-    st.plotly_chart(fig)
+    # Create sequences for LSTM
+    sequence_length = 60
+    X, y = [], []
+    for i in range(len(scaled_data) - sequence_length):
+        X.append(scaled_data[i:i + sequence_length])
+        y.append(scaled_data[i + sequence_length])
+
+    X, y = np.array(X), np.array(y)
+    X = np.reshape(X, (X.shape[0], X.shape[1], 1))
+
+    return X, y, scaler
+
+
+def create_lstm_model(input_shape):
+    model = Sequential()
+    model.add(LSTM(units=50, return_sequences=True, input_shape=input_shape))
+    model.add(LSTM(units=50, return_sequences=False))
+    model.add(Dense(units=25))
+    model.add(Dense(units=1))
+
+    return model
 
 
 def main():
-    # Apply custom CSS style to the slider
-    st.markdown(CUSTOM_CSS, unsafe_allow_html=True)
-
     st.title('Stock Forecast App')
 
     stocks = ('GOOG', 'AAPL', 'MSFT', 'GME')
@@ -101,27 +98,51 @@ def main():
     # Plot raw data
     plot_raw_data(data)
 
-    # Preparing data for Prophet
+    # Preparing data for LSTM
     df_train = data[['Date', 'Close']].rename(columns={"Date": "ds", "Close": "y"})
+    df_train.set_index('ds', inplace=True)
 
-    # Predicting the stock market price using the Prophet library
-    m = Prophet()
-    m.fit(df_train)
-    future = m.make_future_dataframe(periods=period)
-    forecast = m.predict(future)
+    X, y, scaler = prepare_data(df_train)
+
+    # Create LSTM model
+    input_shape = (X.shape[1], 1)
+    model = create_lstm_model(input_shape)
+
+    # Compile and fit the model
+    model.compile(optimizer=Adam(learning_rate=0.001), loss='mean_squared_error')
+    model.fit(X, y, epochs=50, batch_size=32)
+
+    # Forecast using the fitted model
+    last_sequence = X[-1]
+    forecast = []
+    for _ in range(period):
+        next_pred = model.predict(last_sequence.reshape(1, last_sequence.shape[0], 1))
+        forecast.append(next_pred[0][0])
+        last_sequence = np.append(last_sequence[1:], next_pred[0][0])
+
+    # Inverse transform the forecasted data
+    forecast = scaler.inverse_transform(np.array(forecast).reshape(-1, 1))
+
+    # Generate forecast index
+    forecast_index = pd.date_range(df_train.index[-1], periods=period + 1)
+
+    # Remove the last date from the index to make it right-closed
+    forecast_index = forecast_index[:-1]
+
+    # Create a dataframe with forecast data
+    forecast_df = pd.DataFrame({'yhat': forecast.flatten()}, index=forecast_index)
 
     # Display forecast data
     st.subheader('Forecast data')
-    st.write(forecast.tail())
+    st.write(forecast_df.tail())
 
     # Display forecast plot
     st.subheader(f'Forecast plot for {n_years} years')
-    plot_forecast_data(forecast)
-
-    # Display forecast components
-    st.subheader("Forecast components")
-    fig2 = m.plot_components(forecast)
-    st.write(fig2)
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=df_train.index, y=df_train['y'], mode='lines', name="Stock Price", line=dict(color='royalblue')))
+    fig.add_trace(go.Scatter(x=forecast_df.index, y=forecast_df['yhat'], mode='lines', name="Forecast", line=dict(color='darkorange')))
+    fig.update_layout(title_text='Stock Price Forecast', xaxis_title='Date', yaxis_title='Price')
+    st.plotly_chart(fig)
 
 
 if __name__ == "__main__":
